ab81c4fe57d86109156c9d52a9430d1e393ce315
Madalyn Elliott <143676338+melliott3@users.noreply.github.com>
2023-12-02 00:40:28 -0600

    Update script.js
    the added code should help the 4th page run well so that the map is visible

--- script.js
+++ script.js
@@ -21,3 +21,49 @@
         });
     }
 });
+
+function initMap() {
+        // Use geocoding to get coordinates for the address
+        var geocoder = new google.maps.Geocoder();
+        var address = '7102 S Stony Is Ave, Chicago, IL 60649';
+
+        geocoder.geocode({ address: address }, function (results, status) {
+          if (status === 'OK') {
+            var location = results[0].geometry.location;
+
+            var map = new google.maps.Map(
+              document.getElementById('map'),
+              {
+                zoom: 14,
+                center: location,
+              }
+            );
+
+            var marker = new google.maps.Marker({
+              position: location,
+              map: map,
+              icon: 'images/oblock.png',
+              title: 'My favorite food place',
+              animation: google.maps.Animation.DROP,
+              draggable: true,
+            });
+
+            var infoWindow = new google.maps.InfoWindow({
+              content:
+                '<div style="width: 200px; height: 180px;">' +
+                '<h3>My favorite food place</h3>' +
+                '<p>This is my go-to place for good food!</p>' +
+                '</div>',
+            });
+
+            marker.addListener('click', function () {
+              infoWindow.open(map, marker);
+            });
+          } else {
+            alert(
+              'Geocode was not successful for the following reason: ' +
+                status
+            );
+          }
+        });
+      }
